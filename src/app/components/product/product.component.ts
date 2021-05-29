b2ac3ae6ef870b24dda2d46e65d16562f1fa9bee
tanju.avsar <tanju.avsar@asisct.com>
2021-05-30 00:47:06 +0300

    17. Ders 29.05.2021 Commit 3

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { HttpClient } from '@angular/common/http';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
 
 @Component({
   selector: 'app-product',
@@ -7,10 +9,26 @@ import { Product } from 'src/app/models/product';
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-
   products: Product[] = [];
+  apiUrl = 'https://localhost:44345/api/Products/getall';
+  // productResponseModel:ProductResponseModel={
+  //   data:this.products,
+  //   message:"",
+  //   success:true
+  // };
 
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.httpClient
+      .get<ProductResponseModel>(this.apiUrl)
+      .subscribe((response) => {
+        this.products = response.data;
+        console.log(this.products);
+      });
+  }
 }
