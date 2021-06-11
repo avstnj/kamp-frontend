f6720f21770f7595c9185fd59a86e7d74a16df13
tanju.avsar <tanju.avsar@asisct.com>
2021-06-11 21:14:34 +0300

    11.06.2021 19. Ders Son kısım

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -23,7 +24,8 @@ export class ProductComponent implements OnInit {
   constructor(
     private productService: ProductService,
     private activatedRoute: ActivatedRoute,
-    private toastrService: ToastrService
+    private toastrService: ToastrService,
+    private cartService: CartService
   ) {}
 
   ngOnInit(): void {
@@ -52,6 +54,7 @@ export class ProductComponent implements OnInit {
   }
   addToCart(product: Product) {
     console.log(product);
-    this.toastrService.success("Sepete Eklendi",product.productName);
+    this.cartService.addToCart(product);
+    this.toastrService.success('Sepete Eklendi', product.productName);
   }
 }
